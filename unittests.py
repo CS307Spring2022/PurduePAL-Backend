b644de3edc7b7fd00d5d0bf9558d91e10c8a7fe4
Keerthana Vegesna <kvegesna01@gmail.com>
2022-03-25 16:35:53 -0400

    list of unit tests to be implemented

---
+++ unittests.py
@@ -0,0 +1,18 @@
+import unittests
+
+
+# 1. unique username
+# 1a. test username is unique
+# 1b. test unique username added to database
+# 1c. test to check username that already exists
+# 1d. test existing username not added to database
+
+
+# 2. Login 
+# 2a. test correct username correct password
+# 2b. test correct username wrong password
+# 2c. test non existing username
+
+
+# 3. following users
+# 3a. test if number of following users is added to 
