e3224084960efa822d9be8648aa2bf8760fc5753
vsarunhah <varunshah660@gmail.com>
2022-03-26 18:10:58 -0400

    comments are allowed

--- create_post.py
+++ create_post.py
@@ -2,25 +2,26 @@
 
 from helpers import safeget, db
 
-def create_post(data:dict) -> bool:
+
+def create_post(data: dict) -> bool:
 	
 	topic = safeget(data, "topicName")
 	user = safeget(data, "user")
-	timestamp = safeget("timestamp")
+	timestamp = safeget(data, "timestamp")
 	likeCount = 0
 	dislikeCount = 0
 	isComment = False
-	content = safeget("content")
+	content = safeget(data, "content")
 	comments = []
 	commentCount = 0
+	parent_id = safeget(data, "parentID")
 
-	return_val = db["posts"].insert_one({"topic": topic, "user": user, "timestamp": timestamp, "likeCount": likeCount, "dislikeCount": dislikeCount
-	"isComment": isComment, "content": content, "comments": comments, "commentCount": commentCount})
-	
-    if not return_val.acknowledged:
+	return_val = db["posts"].insert_one({"topic": topic, "user": user, "timestamp": timestamp, "likeCount": likeCount, "dislikeCount": dislikeCount,
+	"isComment": isComment, "content": content, "comments": comments, "commentCount": commentCount, "parentID": parent_id})
+
+	if not return_val.acknowledged:
 		return False
 	return True
-
 
 
 #def create_comment()
