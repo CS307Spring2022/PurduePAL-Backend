76accd24e262ef591f800dbfe9938c5996db0cb3
vsarunhah <varunshah660@gmail.com>
2022-03-26 17:10:21 -0400

    idk

--- userVerification.py
+++ userVerification.py
@@ -1,6 +1,5 @@
-import pymongo
 import re
-from helpers import check_password, safeget, db, check_for_data
+from helpers import db
 from error import Errors
 
 
@@ -14,6 +13,7 @@ def checkUsername(username):
     else:
         return
 
+
 def unique_user(data: dict, username: str):
     username_exists = db["users"].find_one(filter={"username": username})
 
@@ -24,11 +24,8 @@ def unique_user(data: dict, username: str):
         # username is unique, proceed with signup
         return True
 
->>>>>>> 5342c0acf7e898d8657b5497e096104362f1f33a
-
 
 def checkEmail(email):
-<<<<<<< HEAD
     if not re.search("^\S+@purdue.edu$", email):
         return Errors.EMAIL_ERROR
 
@@ -41,20 +38,3 @@ def checkPasswordLength(password):
 def confirmPassword(password, confirmation):
     if password != confirmation:
         return Errors.PASSWORD_ERROR
-=======
-    if not email.contains("^\S+@purdue.edu$"):
-        return Errors.EMAIL_ERROR
-        print("Please use a valid purdue.edu email.")
-
-
-def checkPasswordLength(password):
-    if len(password) < 8:
-        return Errors.PASSWORD_ERROR
-        print("Password must be at least 8 characters long.")
-
-
-def confirmPassword(password, confirmation):
-    if password != confirmation:
-        return Errors.PASSWORD_ERROR
-        print("Passwords don't match!")
->>>>>>> 5342c0acf7e898d8657b5497e096104362f1f33a
