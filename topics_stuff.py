d8f126f459d9558221ba4107c40a811aa9c34b33
vsarunhah <varunshah660@gmail.com>
2022-04-23 16:04:47 -0400

    topic posts

--- topics_stuff.py
+++ topics_stuff.py
@@ -1,8 +1,9 @@
 import json
+from typing import Tuple, List, Any
 
 from bson import json_util
 
-from helpers import db
+from helpers import db, safeget
 
 
 def get_topics() -> list:
@@ -16,3 +17,49 @@ def get_topics() -> list:
 
     topics_dict = sorted(topics_dict, key=lambda x: len(x["posts"]) + len(x["usersFollowing"]), reverse=True)
     return topics_dict
+
+
+def get_topic_posts(data: dict) -> Tuple[List[Any], bool]:
+    user_id = safeget(data, "email", default="anonymous@purdue.edu")
+    user = [u for u in db["users"].find({"_id": user_id})]
+    if (len(user) == 0):
+        return [{"val": "hi"}], False
+    user = user[0]
+    topic_name = safeget(data, "topic")
+
+    likedPosts = [json.loads(json_util.dumps(post["post"]))["$oid"] for post in user["likedPosts"]]
+    dislikedPosts = [json.loads(json_util.dumps(post["post"]))["$oid"] for post in user["dislikedPosts"]]
+    savedPosts = [str(post) for post in user["savedPosts"]]
+
+    posts_cursor = db["posts"].find({"topic": topic_name})
+    posts_dict = []
+    for post in posts_cursor:
+        if (post["contentType"] == 0):
+            post["_id"] = json.loads(json_util.dumps(post["_id"]))["$oid"]
+            post["reactionType"] = 0
+            post["isSaved"] = False
+            if (post["_id"] in likedPosts):
+                post["reactionType"] = 1
+            elif (post["_id"] in dislikedPosts):
+                post["reactionType"] = 2
+
+            if (post['_id'] in savedPosts):
+                post["isSaved"] = True
+
+            if (post["parentID"]):
+                post["parentID"] = json.loads(json_util.dumps(post["parentID"]))["$oid"]
+            for i in range(len(post["comments"])):
+                post["comments"][i] = json.loads(json_util.dumps(post["comments"][i]))["$oid"]
+            posts_dict.append(post)
+
+            poster = [u for u in db["users"].find({"_id": post["user"]})][0]
+            poster = {
+                "username": poster["username"],
+                "email": poster["_id"],
+                "firstName": poster["firstName"],
+                "lastName": poster["lastName"],
+                "public": poster["public"]
+            }
+            post["user"] = poster
+
+    return posts_dict, True
